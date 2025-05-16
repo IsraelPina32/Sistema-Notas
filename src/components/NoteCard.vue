760617924253207b3db0e26cb450f3af6892f839
Israel Pina <israelpina047@gmail.com>
2025-05-16 16:23:59 -0300

    feat:(NoteCard.vue) adicionando novas funcionalidades ao componente NoteCard.

---
+++ src/components/NoteCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="bg-neutral-100 border border-neutral-300  rounded-2xl p-4 mb-4 shadow-sm transition md:shadow-md">
+        <div class="flex justify-between items-start">
+            <h2 class="text-lg font-semibold text-neutral-700 dark:text-neutal-100 break-words">
+                {{ note.title }}
+            </h2>
+            <div class="flex gap-2">
+                <button @click="$emit('edit', note.id)"
+                    class="text-gray-800 pl-2"
+                    title="Editar"
+                >
+                    +
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        note: {
+            type: Object,
+            required: true,
+        }
+    })
+</script>

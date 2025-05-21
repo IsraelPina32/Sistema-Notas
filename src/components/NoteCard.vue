8f3b7fd9a76557e48a79ad18f7e5966d5e61dcd8
Israel Pina <israelpina047@gmail.com>
2025-05-21 19:55:31 -0300

    feat(Components): adicionando novas funcionalidades ao componente.

--- src/components/NoteCard.vue
+++ src/components/NoteCard.vue
@@ -1,17 +1,15 @@
 <template>
-    <div class="bg-neutral-100 border border-neutral-300  rounded-2xl p-4 mb-4 shadow-sm transition md:shadow-md">
-        <div class="flex justify-between items-start">
+    <div class="bg-neutral-100 border border-neutral-300  rounded-2xl p-5 mb-5 shadow-sm transition md:shadow-md flex justify-center items-center">
+        <div class="flex p-2 min-w-full   justify-between items-center gap-2">
             <h2 class="text-lg font-semibold text-neutral-700 dark:text-neutal-100 break-words">
-                {{ note.title }}
+                {{ note.title }} :
             </h2>
-            <div class="flex gap-2">
-                <button @click="$emit('edit', note.id)"
-                    class="text-gray-800 pl-2"
-                    title="Editar"
-                >
-                    +
-                </button>
-            </div>
+            <p class="text-neutral-700">{{ note.content }}</p>
+            <p class="text-xs text-neutral-700">{{ note.createdAt }}</p>
+        </div>
+        <div class="flex gap-2 ">
+            <button @click="$emit('edit', note)" class="text-stone-500">Editar</button>
+             <button @click="$emit('delete', note)" class="text-stone-500">Excluir</button>
         </div>
     </div>
 </template>
@@ -23,4 +21,5 @@
             required: true,
         }
     })
+    defineEmits(['edit', 'delete'])
 </script>

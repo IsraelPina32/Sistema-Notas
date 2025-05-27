3680cac19cda70e1a06e2ffef47924b3cff3ff3e
Israel Pina <israelpina047@gmail.com>
2025-05-27 18:51:37 -0300

    style:(component): adicionando novos estilos para meu componente.

--- src/components/NoteCard.vue
+++ src/components/NoteCard.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="bg-neutral-100 border border-neutral-300  rounded-2xl p-5 mb-5 shadow-sm transition md:shadow-md flex justify-center items-center">
-        <div class="flex p-2 min-w-full   justify-between items-center gap-2">
-            <h2 class="text-lg font-semibold text-neutral-700 dark:text-neutal-100 break-words">
+    <div class="bg-neutral-100 border border-neutral-300  rounded-2xl p-7 mb-6 shadow-sm transition md:shadow-md flex justify-center items-center w-md">
+        <div class="flex-1 p-1">
+            <h2 class="text-lg font-semibold text-neutral-700 dark:text-neutral-100 break-words">
                 {{ note.title }} :
             </h2>
             <p class="text-neutral-700">{{ note.content }}</p>
-            <p class="text-xs text-neutral-700">{{ note.createdAt }}</p>
+            <p class="text-xs text-neutral-700 font-mono ">{{ note.createdAt }}</p>
         </div>
-        <div class="flex gap-2 ">
+        <div class="flex flex-col gap-2 p-2">
             <button @click="$emit('edit', note)" class="text-stone-500">Editar</button>
              <button @click="$emit('delete', note)" class="text-stone-500">Excluir</button>
         </div>

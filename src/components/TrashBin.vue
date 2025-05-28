e4c5d0f620dda1c3377798f6674b83793a224988
Israel Pina <israelpina047@gmail.com>
2025-05-28 17:04:22 -0300

    feat:(TrashBin) adicionando nova funcionalidade para restaura um número limitado de notas para o projeto.

---
+++ src/components/TrashBin.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1 class="text-xl font-bold mt-6">Lixeira</h1>
+    <div v-if="trash.legth === 0" class="text-neutral-500">Sem notas na Lixeiras.</div>
+        <div v-for="note in trash" :key="note.id" class="border p-4 mt-2 rounded">
+            <h3 class="font-bold">{{ note.title }}</h3>
+            <p>{{ note.content }}</p>
+            <button @click="restoreFromTrash(note)" class="text-stone-500">Restaurar Nota</button>
+        </div>
+ 
+</template>

9cf51bc581d8139b396088011bf4bbafe8c3c353
Israel Pina <israelpina047@gmail.com>
2025-05-28 16:35:08 -0300

    style:(NoteCard.vue): Adicioando um novo estilo ao meu componente.

--- src/components/NoteCard.vue
+++ src/components/NoteCard.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="bg-neutral-100 border border-neutral-300  rounded-2xl p-7 mb-6 shadow-sm transition md:shadow-md flex justify-center items-center w-md">
         <div class="flex-1 p-1">
-            <h2 class="text-lg font-semibold text-neutral-700 dark:text-neutral-100 break-words">
-                {{ note.title }} :
+            <h2 class="text-lg font-semibold text-neutral-700  break-words">
+                {{ note.title }}.
             </h2>
             <p class="text-neutral-700">{{ note.content }}</p>
             <p class="text-xs text-neutral-700 font-mono ">{{ note.createdAt }}</p>

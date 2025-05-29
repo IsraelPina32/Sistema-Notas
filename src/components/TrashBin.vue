3acdf0d32d9746eac64789bdf519faaf941f4cc1
Israel Pina <israelpina047@gmail.com>
2025-05-29 13:01:09 -0300

    feat:(TrashBin.vue) Adicionando um novo botão para excluir permanentemente as notas, deixando apenas 10 notas no LocalStorage.

--- src/components/TrashBin.vue
+++ src/components/TrashBin.vue
@@ -1,11 +1,16 @@
 <template>
     <h1 class="text-xl font-bold mt-6 text-neutral-700 ">Lixeira</h1>
-    <div v-if="trash.length === 0" class="text-neutral-500 ">Sem notas na Lixeiras.</div>
+    <div v-if="trash.length === 0" class="text-neutral-600 ">Sem notas na sua Lixeiras.</div>
         <div v-for="note in trash" :key="note.id" class="p-4 mt-2 rounded-2xl bg-neutral-100 border border-neutral-300 shadow-sm transition md:shadow-md flex justify-center items-center flex-col w-md">
             <h3 class="font-bold text-neutral-700 ">{{ note.title }}</h3>
             <p class="text-neutral-700 ">{{ note.content }}</p>
             <button @click="emit('restore', note.id)" class="bg-neutral-100 border border-neutral-300 text-neutral-700  mb-2 p-1 rounded-2xl">Restaurar Nota</button>
+            <button @click="emit('permanent-delete', note.id)" class="bg-neutral-100 border border-neutral-300 text-neutral-700  mb-2 p-1 rounded-2xl">Excluir Permanentemente</button>
         </div>
+
+        <p v-if="trash.length > 10" class="text-xm text-neutral-500 mt-4">
+            Só podem ser exibidas apenas 10 notas recentes da Lixeira.
+        </p>
  
 </template>
 
@@ -17,5 +22,5 @@
         }
     })
 
-    const emit = defineEmits(['restore'])
+    const emit = defineEmits(['restore', 'permanent-delete'])
 </script>

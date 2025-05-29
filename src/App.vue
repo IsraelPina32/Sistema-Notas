968e8bf9041f11dc5994dd41ff740dfa2ea78324
Israel Pina <israelpina047@gmail.com>
2025-05-29 13:51:57 -0300

    refactor:(App.vue) refatorando o meu app.vue e deixando mais organizando e adicionando uma nova função para remover permanentemente minha lixeira.

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,15 @@
     <section class="max-w-2xl mx-auto p-4">
         <h1 class="text-2xl font-bold mb-4 text-neutral-800">Minhas Notas</h1>
 
-        <form @submit.prevent="addNote">
-            <div>
-                <label class="block text-neutral-700 mb-2">Titulo da Nota</label>
-                <input v-model="newNote.title" type="text" class="border p-1 w-full text-neutral-700 bg-neutral-100 border-neutral-300  mb-2" placeholder="Digite a sua nota">
-            </div>
-            <div>
-                <label class="block text-neutral-700  mb-2">Conteúdo da Nota</label>
-                <textarea v-model="newNote.content" class="border p-1 w-full bg-neutral-100  border-neutral-300 text-neutral-700 mb-2 rounded-2xl"></textarea>
-            </div>
-            <button type="submit" class="bg-neutral-100 border border-neutral-300 text-neutral-700  mb-2 p-1 rounded-2xl">
-                {{  isEditing ? 'Salvar' : 'Adicionar' }}
-            </button>
-        </form>
-
+        <NoteForm :note="newNote" :isEditing="isEditing" @submit="addNote"/>
         <NoteCard v-for="note in notes" :note="note" :key="note.id" @edit="handleEdit" @delete="handleDelete" />
-        <TrashBin :trash="trash" @restore="restoreFromTrash"/>
+        <TrashBin :trash="trash" @restore="restoreFromTrash" @permanent-delete="permanentlyDeleteNote"/>
     </section>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
+import NoteForm from './components/NoteForm.vue'
 import NoteCard from './components/NoteCard.vue'
 import TrashBin from './components/TrashBin.vue'
 
@@ -34,16 +22,16 @@ const newNote = ref({ title: '', content: '' })
 const isEditing = ref(false)
 const editId = ref(null)
 
-const addNote = () => {
-    if (!newNote.value.title || !newNote.value.content) return
+const addNote = (noteData) => {
+    if (!noteData.title || !noteData.content) return
 
     if (isEditing.value) {
         const index = notes.value.findIndex(note => note.id === editId.value)
         if (index !== -1) {
             notes.value[index] = {
                 ...notes.value[index],
-                title: newNote.value.title,
-                content: newNote.value.content,
+                title: noteData.title,
+                content: noteData.content,
             }
         }
         isEditing.value = false
@@ -51,8 +39,8 @@ const addNote = () => {
     } else {
         notes.value.push({
             id: Date.now(),
-            title: newNote.value.title,
-            content: newNote.value.content,
+            title: noteData.title,
+            content: noteData.content,
             createdAt: new Date().toISOString().slice(0, 10),
         })
     }
@@ -85,6 +73,11 @@ const restoreFromTrash = (noteID) => {
         notes.value.push(note)
         saveNotes()
     }
+}
+
+const permanentlyDeleteNote = (noteId) => {
+    trash.value = trash.value.filter(n => n.id !== noteId)
+    saveNotes()
 }
 
 const saveNotes = () => {

3b8e2bd85b5b352d34e08edd180a1a7a2b70314f
Israel Pina <israelpina047@gmail.com>
2025-06-09 16:08:37 -0300

    feat:(NoteList.vue) transferindo os componennte editar e deletar para um componente para futuramente ele ser reutilizado para o Projeto.

---
+++ src/components/NoteList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="grid gap-4 mt-4">
+        <NoteCard
+            v-for="note in notes"
+            :key="note.id"
+            :note="note"
+            @edit="handleEdit"
+            @delete="handleDelete"
+        />
+    </div>
+</template>
+
+<script setup>
+import NoteCard from './NoteCard.vue';
+
+const props = defineProps({
+    notes: {
+        type: Array,
+        required: true
+    }
+})
+
+
+const emit = defineEmits(['edit', 'delete'])
+
+const handleEdit = (note) => {
+    emit('edit', note);
+}
+
+const handleDelete = (note) => {
+    emit('delete', note)
+}
+</script>

32b857ac532eed0e13a54206b13ac884c381b9b1
Israel Pina <israelpina047@gmail.com>
2025-06-12 16:20:55 -0300

    refactor:(NoteForm) refatorando o meu componente para o modo Dark.

--- src/components/NoteForm.vue
+++ src/components/NoteForm.vue
@@ -1,14 +1,14 @@
 <template>
      <form @submit.prevent="handleSubmit">
             <div>
-                <label class="block text-neutral-700 mb-2">Titulo da Nota</label>
-                <input v-model="form.title" type="text" class="border p-1 w-full text-neutral-700 bg-neutral-100 border-neutral-300  mb-2" placeholder="Digite a sua nota">
+                <label class="block dark:text-gray-400 text-gray-500 mb-3">Titulo da Nota</label>
+                <input v-model="form.title" type="text" class="border p-2 w-full text-neutral-700 bg-neutral-100 border-neutral-300 mb-2  rounded-2xl" placeholder="Digite o titulo da sua nota.">
             </div>
             <div>
-                <label class="block text-neutral-700  mb-2">Conteúdo da Nota</label>
-                <textarea v-model="form.content" class="border p-1 w-full bg-neutral-100  border-neutral-300 text-neutral-700 mb-2 rounded-2xl"></textarea>
+                <label class="block dark:text-gray-400 text-gray-500 mb-3">Conteúdo da Nota</label>
+                <textarea v-model="form.content" class="border p-1 w-full bg-neutral-100  border-neutral-300 text-neutral-700 mb-2 rounded-2xl" placeholder="Digite a sua nota"></textarea>
             </div>
-            <button type="submit" class="bg-neutral-100 border border-neutral-300 text-neutral-700  mb-2 p-1 rounded-2xl">
+            <button type="submit" class="bg-neutral-100 border border-neutral-300 text-neutral-700  mb-2 p-2 rounded-2xl">
                 {{  isEditing ? 'Salvar' : 'Adicionar' }}
             </button>
         </form>

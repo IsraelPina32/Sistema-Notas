7cd020826efea2fd55de71daec53522f12be0cb5
Israel Pina <israelpina047@gmail.com>
2025-06-12 16:21:30 -0300

    refactor:(TrashBin.vue) refatorando o meu componente para o modo Dark.

--- src/components/TrashBin.vue
+++ src/components/TrashBin.vue
@@ -1,6 +1,6 @@
 <template>
-    <h1 class="text-xl font-bold mt-6 text-neutral-700 ">Lixeira</h1>
-    <div v-if="trash.length === 0" class="text-neutral-600 ">Sem notas na sua Lixeiras.</div>
+    <h1 class="text-xl font-bold mt-6 dark:text-gray-400 text-gray-500 ">Lixeira</h1>
+    <div v-if="trash.length === 0" class="dark:text-gray-400 text-gray-500">Sem notas na sua Lixeiras.</div>
         <div v-for="note in trash" :key="note.id" class="p-4 mt-2 rounded-2xl bg-neutral-100 border border-neutral-300 shadow-sm transition md:shadow-md flex justify-center items-center flex-col w-md">
             <h3 class="font-bold text-neutral-700 ">{{ note.title }}</h3>
             <p class="text-neutral-700 ">{{ note.content }}</p>
@@ -8,7 +8,7 @@
             <button @click="emit('permanent-delete', note.id)" class="bg-neutral-100 border border-neutral-300 text-neutral-700  mb-2 p-1 rounded-2xl">Excluir Permanentemente</button>
         </div>
 
-        <p v-if="trash.length > 10" class="text-xm text-neutral-500 mt-4">
+        <p v-if="trash.length > 10" class="text-xm font-bold dark:text-gray-400 text-gray-500 mt-4">
             Só podem ser exibidas apenas 10 notas recentes da Lixeira.
         </p>
  

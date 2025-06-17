fa1c35d8ecbb92b15f4a46ba6d69e81fdadf9a17
Israel Pina <israelpina047@gmail.com>
2025-06-16 21:09:37 -0300

    feat:(Componentes): novos icones componentes para o modo Dark do projeto.

---
+++ src/components/icons/MoonIcon.vue
@@ -0,0 +1,5 @@
+<template>
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-neutral-100 dark:text-neutral-100 transition-colors duration-300" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+      <path d="M21 12.79A9 9 0 1111.21 3a7 7 0 109.79 9.79z" />
+    </svg>
+</template>
---
+++ src/components/icons/SunIcon.vue
@@ -0,0 +1,11 @@
+<template>
+
+    <svg xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6 text-gray-500 dark:text-gray-500 transition-colors duration-300" viewBox="0 0 24 24"
+        fill="none" stroke="currentColor" stroke-width="2">
+        <circle cx="12" cy="12" r="4" />
+        <path
+            d="M12 2v2M12 20v2M4.93 4.93l1.41 1.41M17.66 17.66l1.41 1.41M2 12h2M20 12h2M4.93 19.07l1.41-1.41M17.66 6.34l1.41-1.41" />
+    </svg>
+
+</template>

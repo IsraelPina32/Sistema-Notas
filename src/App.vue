b6855d9eb7e7f3f36b3757d52d0473b4c18db312
Israel Pina <israelpina047@gmail.com>
2025-06-16 21:11:52 -0300

    feat:(App.vue) adicionando o novo modo Dark com com novas animações e icones personalizados.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
-    <section class="max-w-2xl mx-auto p-4">
-        <h1 class="text-2xl font-bold mb-4 text-neutral-800">Minhas Notas</h1>
+    <section class="fixed  top-4 right-90 z-50">
 
-        <NoteForm :note="newNote" :isEditing="isEditing" @submit="addNote"/>
-     
-        <NoteList
-            :notes="notes"
-            @edit="handleEdit"
-            @delete="handleDelete"
-        />
-        <TrashBin :trash="trash" @restore="restoreFromTrash" @permanent-delete="permanentlyDeleteNote"/>
+        <button @click="toogleDark" class="flex items-center gap-2 p-2">
+            <Transition name="fade" mode="out-in">
+                <SunIcon v-if="!isDark" key="sun" />
+                <MoonIcon v-else key="moon" />
+            </Transition>
+        </button>
+    </section>
+
+    <section class="max-w-full mx-auto p-4">
+
+        <h1 class="text-3xl font-bold m-2 text-gray-600 dark:text-neutral-200">Minhas Notas</h1>
+
+        <NoteForm :note="newNote" :isEditing="isEditing" @submit="addNote" />
+        <NoteList :notes="notes" @edit="handleEdit" @delete="handleDelete" />
+        <TrashBin :trash="trash" @restore="restoreFromTrash" @permanent-delete="permanentlyDeleteNote" />
     </section>
 </template>
 
@@ -17,7 +23,32 @@
 import NoteForm from './components/NoteForm.vue'
 import NoteList from './components/NoteList.vue'
 import TrashBin from './components/TrashBin.vue'
-import { useNotes} from './composables/useNotes.js'
+import { useNotes } from './composables/useNotes.js'
+import { useDarkMode } from './composables/useDarkMode.js'
+import MoonIcon from './components/icons/MoonIcon.vue'
+import SunIcon from './components/icons/SunIcon.vue'
+import { onMounted, ref, Transition, watch } from 'vue'
+
+const { isDark } = useDarkMode();
+const lottieRef = ref(null);
+
+
+onMounted(() => {
+    watch(isDark, (val) => {
+        document.documentElement.classList.toggle('dark', val);
+    }, { immediate: true })
+})
+
+const toogleDark = () => {
+    isDark.value = !isDark.value;
+
+    setTimeout(() => {
+        if (lottieRef.value) {
+            lottieRef.value.play();
+        }
+    }, 100);
+
+}
 
 const {
     notes,

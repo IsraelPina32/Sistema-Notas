ed636d59f22a5a8e5ceefa2ed89526e14df024db
Israel Pina <israelpina047@gmail.com>
2025-06-18 17:41:15 -0300

    feat(App.vue) adicionando novo funcionalidade, um filtro para ter as notas mais destacadas para o usuário.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,35 @@
 <template>
-    <section class="fixed  top-4 right-90 z-50">
+    <div class="fixed top-4 left-1/2 z-50 mb-4 w-full max-w-lg px-4 -translate-x-1/2 transform">
+        <div class="relative">
+            <input
+                   ref="searchInput"
+                   v-model="searchQuery"
+                   type="text"
+                   placeholder="🔍 Buscar sua nota por título, conteúdo ou data"
+                   class="w-full p-2 rounded-xl border-neutral-300 mb-4 bg-neutral-100 text-gray-800 shadow-md  transition focus:ring-2 focus-ring-gray-500"
+               />
+            <span class="text-xs text-gray-400 dark:text-neutral-500 absolute right-5 top-1/3 -translate-y-1.75">
+                Ctrl + K
+            </span>
+        </div>
 
+    </div>
+    <div class="fixed  top-4 right-90 z-50">
+       
         <button @click="toogleDark" class="flex items-center gap-2 p-2">
             <Transition name="fade" mode="out-in">
                 <SunIcon v-if="!isDark" key="sun" />
                 <MoonIcon v-else key="moon" />
             </Transition>
         </button>
-    </section>
+    </div>
 
     <section class="max-w-full mx-auto p-4">
 
         <h1 class="text-3xl font-bold m-2 text-gray-600 dark:text-neutral-200">Minhas Notas</h1>
 
         <NoteForm :note="newNote" :isEditing="isEditing" @submit="addNote" />
-        <NoteList :notes="notes" @edit="handleEdit" @delete="handleDelete" />
+        <NoteList :notes="filteredNotes" @edit="handleEdit" @delete="handleDelete" />
         <TrashBin :trash="trash" @restore="restoreFromTrash" @permanent-delete="permanentlyDeleteNote" />
     </section>
 </template>
@@ -27,13 +42,43 @@ import { useNotes } from './composables/useNotes.js'
 import { useDarkMode } from './composables/useDarkMode.js'
 import MoonIcon from './components/icons/MoonIcon.vue'
 import SunIcon from './components/icons/SunIcon.vue'
-import { onMounted, ref, Transition, watch } from 'vue'
+import { onMounted, ref, Transition, watch,computed, onUnmounted } from 'vue'
 
 const { isDark } = useDarkMode();
+
+const searchQuery = ref('');
+const searchInput = ref(null);
+
 const lottieRef = ref(null);
 
 
+
+const filteredNotes = computed(() => {
+
+    if(!searchQuery.value.trim()) return notes.value;
+
+    const query = searchQuery.value.toLocaleLowerCase();
+    return notes.value.filter(note => {
+        return note.title.toLowerCase().includes(query) ||
+            note.content.toLowerCase().includes(query) ||
+            note.createdAt.toLowerCase().includes(query);
+    })
+})
+
+const handleShortCut = (event) => {
+    if (event.ctrlKey && event.key === 'k') {
+        event.preventDefault();
+        searchInput.value?.focus()
+    }
+}
+
+onUnmounted(() => window.removeEventListener('keydown', handleShortCut))
+
+
 onMounted(() => {
+
+    window.addEventListener('keydown', handleShortCut);
+
     watch(isDark, (val) => {
         document.documentElement.classList.toggle('dark', val);
     }, { immediate: true })
@@ -61,4 +106,5 @@ const {
     restoreFromTrash,
     permanentlyDeleteNote
 } = useNotes();
+
 </script>

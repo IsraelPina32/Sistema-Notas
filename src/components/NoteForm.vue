13311b0407790a3f2c55ee873c05eefa035aae1c
Israel Pina <israelpina047@gmail.com>
2025-09-03 22:05:18 -0300

    style(noteForm): acdicionando o grid para deixar o layout mais responsivo para o mobile  no componente NoteForm.

--- src/components/NoteForm.vue
+++ src/components/NoteForm.vue
@@ -1,16 +1,18 @@
 <template>
-     <form @submit.prevent="handleSubmit">
-            <div>
-                <label class="block dark:text-gray-400 text-gray-500 mb-3 font-medium">Titulo da Nota</label>
+     <form @submit.prevent="handleSubmit" class="w-full max-w-3xl mx-auto grid gap-5 sm:gap-6 sm:grid-cols-2">
+            <div class="sm:col-span-2">
+                <label class="block dark:text-gray-400 text-gray-500 mb-3 font-medium text-sm sm:text-base">Titulo da Nota</label>
                 <input v-model="form.title" type="text" class="border p-2.5 w-full text-neutral-800 bg-neutral-100 border-neutral-300 mb-3  rounded-2xl" placeholder="Digite o titulo da sua nota.">
             </div>
-            <div>
-                <label class="block dark:text-gray-400 text-gray-500 mb-3 font-medium">Conteúdo da Nota</label>
+            <div class="sm:col-span-2">
+                <label class="block dark:text-gray-400 text-gray-500 mb-3 font-medium text-sm sm:text-base">Conteúdo da Nota</label>
                 <textarea v-model="form.content" class="border p-2.5 w-full bg-neutral-100  border-neutral-300 text-neutral-800 mb-2 rounded-2xl" placeholder="Digite a sua nota"></textarea>
             </div>
-            <button type="submit" class="bg-neutral-100 border border-neutral-300 text-neutral-700  mb-2 p-2.5 rounded-3xl">
-                {{  isEditing ? 'Salvar' : 'Adicionar' }}
-            </button>
+            <div class="sm:col-span-2 flex justify-center">
+                <button type="submit" class="bg-neutral-100 border border-neutral-300 text-neutral-700  mb-2 p-2.5 rounded-3xl">
+                    {{  isEditing ? 'Salvar' : 'Adicionar' }}
+                </button>
+            </div>
         </form>
 
 </template>

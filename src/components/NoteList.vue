5485f2269b6b077b9c27481f5193f8c6b77e6ba5
Israel Pina <israelpina047@gmail.com>
2025-09-08 18:22:53 -0300

    feat(NoteList): adicionando nova função para o card para emitir o card unico.

--- src/components/NoteList.vue
+++ src/components/NoteList.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="grid  gap-6 mt-4 grid-cols=[repeat(auto-fit,minmax(250px,1fr))]">
-        <NoteCard
-            v-for="note in notes"
-            :key="note.id"
-            :note="note"
-            @edit="handleEdit"
-            @delete="handleDelete"
-            @favorite="handleFavorite"
+    <div v-if="!selectedNote" class="grid  gap-6 mt-4 grid-cols-[repeat(auto-fit,minmax(250px,1fr))]">
+        <NoteCard v-for="note in notes" :key="note.id" 
+        :note="note" 
+        @edit="handleEdit" 
+        @delete="handleDelete"
+        @favorite="handleFavorite"
+        @open="openNote" 
         />
-    </div>
+     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import NoteCard from './NoteCard.vue';
 
 const props = defineProps({
@@ -21,16 +21,10 @@ const props = defineProps({
     }
 })
 
-const emit = defineEmits(['edit', 'delete', 'favorite'])
-
-const handleEdit = (note) => {
-    emit('edit', note);
-}
-
-const handleDelete = (note) => {
-    emit('delete', note)
-}
-
+const emit = defineEmits(['edit', 'delete', 'favorite', 'open'])
+const handleEdit = (note) => emit('edit', note);
+const handleDelete = (note) => emit('delete', note)
 const handleFavorite = (id) => emit('favorite', id);
+const openNote = (note) => emit('open', note);
 
 </script>
